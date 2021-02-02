951f7dd0d8d67d67a522852058945e0439586e6b
Japheth Ishola <japhethish@gmail.com>
2021-02-02 13:39:35 -0800

    Added React Router to App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,27 +1,33 @@
 import logo from '../assets/logo.svg';
 import style from './App.module.scss';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Group from '../components/Group/Group';
 
 function App() {
   return (
-    <div className={style.App}>
-      <header className={style.header}>
-        <img src={logo} className={style.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Router>
+
+      <div className={style.App}>
+        <header className={style.header}>
+          <img src={logo} className={style.logo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className={style.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className={style.link}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-        <Group />
-      </header>
-    </div>
+          <Group />
+        </header>
+      </div>
+      
+    </Router>
   );
 }
 
