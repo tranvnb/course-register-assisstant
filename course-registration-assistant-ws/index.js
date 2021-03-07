94ccd3f1742331ecf0cc45e5a5c332c0a7494130
Japheth Ishola <japhethish@gmail.com>
2021-03-06 20:02:46 -0800

    Added Dockerfile for the Web service/api

--- course-registration-assistant-ws/index.js
+++ course-registration-assistant-ws/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send("The course registration assistant web service is running");
+  res.send("The course registration assistant api is running");
 });
 
 app.listen(port, () => {
